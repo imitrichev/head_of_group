6edca52aee377089441c9efdce50beff629dd1b1
Nikita <anto853@mail.ru>
2023-09-13 16:08:51 +0300

    добавлен ввод фамилий кандидатов

--- main.cpp
+++ main.cpp
@@ -7,32 +7,34 @@
 // Структура для хранения информации о кандидате
 struct Candidate {
     std::string name;
+    std::string surname;
     int votes;
 
-    Candidate(std::string n) {
+    Candidate(std::string n, std::string s) {
         name = n;
+        surname = s;
         votes = 0;
     }
 };
 
 // Функция для проведения голосования
 void conductElection(std::vector<Candidate>& candidates) {
-    std::cout << "Голосование за выбор старосты!" << std::endl;
+    std::cout << "Elections of captain!" << std::endl;
 
     // Вывод списка кандидатов
-    std::cout << "Кандидаты:" << std::endl;
+    std::cout << "Candidates:" << std::endl;
     for (int i = 0; i < candidates.size(); i++) {
-        std::cout << i + 1 << ". " << candidates[i].name << std::endl;
+        std::cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name << std::endl;
     }
 
     // Цикл голосования
     while (true) {
-        std::cout << "Введите номер кандидата, за которого хотите проголосовать (0 - закончить голосование): ";
+        std::cout << "Enter number of candidate, you want to vote for (0 - end elections): ";
         int choice;
         std::cin >> choice;
 
         if (choice < 0 || choice > candidates.size()) {
-            std::cout << "Недопустимый выбор!" << std::endl;
+            std::cout << "Unavailable choice!" << std::endl;
             continue;
         }
         else if (choice == 0) {
@@ -49,25 +51,27 @@ void conductElection(std::vector<Candidate>& candidates) {
     });
 
     // Вывод результатов голосования
-    std::cout << "Результаты голосования:" << std::endl;
+    std::cout << "Election results:" << std::endl;
     for (int i = 0; i < candidates.size(); i++) {
-        std::cout << i + 1 << ". " << candidates[i].name << ": " << candidates[i].votes << " голосов" << std::endl;
+        std::cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name << ": " << candidates[i].votes << " votes" << std::endl;
     }
 }
 
 int main() {
     // Ввод количества кандидатов
     int numCandidates;
-    std::cout << "Введите количество кандидатов: ";
+    std::cout << "Enter candidates amount: ";
     std::cin >> numCandidates;
 
-    // Ввод имён кандидатов
+    // Ввод имён и фамилий кандидатов
     std::vector<Candidate> candidates;
     for (int i = 0; i < numCandidates; i++) {
-        std::string name;
-        std::cout << "Введите имя кандидата #" << i + 1 << ": ";
+        std::string name, surname;
+        std::cout << "Enter candidate #" << i + 1 << " name: ";
         std::cin >> name;
-        candidates.push_back(Candidate(name));
+        std::cout << "Enter candidate #" << i + 1 << " surname: ";
+        std::cin >> surname;
+        candidates.push_back(Candidate(name, surname));
     }
 
     // Проведение голосования
