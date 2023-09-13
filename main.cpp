a0eabcbb0619a447e7d36cc28a6e17dd55394a6d
Nikita <anto853@mail.ru>
2023-09-13 17:23:04 +0300

    в функции добалвена отработка ошибок и тесты к ним

--- main.cpp
+++ main.cpp
@@ -29,9 +29,24 @@ std::string conductElection(std::vector<Candidate>& candidates, std::vector<int>
         std::cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name << std::endl;
     }
 
+    if (candidates.size() === 0) {
+        return "NO CANDIDATES";
+    }
+    if (candidates.size() === 1) {
+        return "NO ELECTIONS";
+    }
+    if (votes.size() === 0) {
+        return "NO VOTES";
+    }
+
+    
+
     // Цикл голосования
     for (int i = 0; i < votes.size(); i++) {
         int choice = votes[i];
+        if (choice > candidates.size()) {
+            return 'ILLEGAL VOTE';
+        }
 
         // Увеличение количества голосов кандидата
         candidates[choice - 1].votes++;
@@ -54,9 +69,48 @@ std::string conductElection(std::vector<Candidate>& candidates, std::vector<int>
 
 
 TEST(FunctionTesting, test_elections) {
+    // тест 1
     std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
     std::vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("Sidorenko Nikita", conductElection(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, test_elections_single_candidate) {
+    // тест 2
+    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko") };
+    std::vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
+    EXPECT_EQ("NO ELECTIONS", conductElection(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, test_elections_no_candidates) {
+    // тест 3
+    std::vector<Candidate> candidates;
+    std::vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
+    EXPECT_EQ("NO CANDIDATES", conductElection(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, test_elections_no_votes) {
+    // тест 4
+    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+    std::vector<int> votes;
+    EXPECT_EQ("NO VOTES", conductElection(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, test_elections_illegal_votes) {
+    // тест 5
+    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+    std::vector<int> votes  = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1, 50, 46, 78};
+    EXPECT_EQ("ILLEGAL VOTE", conductElection(candidates, votes));
+    candidates.clear();
+    votes.clear();
 }
 
 
