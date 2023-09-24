4d59be68da215cc54334f76a1d443348860f36ee
Leo Kazantsev <leokazantsev19@mail.ru>
2023-09-24 17:36:14 +0300

    added tests and prepared project to github actions

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
+#include "gtest/gtest.h"
+#include <math.h>
 using namespace std;
 
 
-// Структура для хранения информации о кандидате
+// СТРУКТУРА ДЛЯ ХРАНЕНИЯ КАНДИДАТА
 struct Candidate {
     string name;
     int votes;
@@ -16,8 +17,10 @@ struct Candidate {
     }
 };
 
-// Функция для проведения голосования
-bool conductElection(vector<Candidate>& candidates) {
+
+// ФУНКЦИЯ ДЛЯ ПРОВЕДЕНИЯ ГОЛОСОВАНИЯ
+bool conductElection(vector<Candidate>& candidates) 
+{
     cout << "voting for the election of the headman!" << endl;
 
     // Вывод списка кандидатов
@@ -60,6 +63,102 @@ bool conductElection(vector<Candidate>& candidates) {
     return true;
 }
 
+
+// ФУНКЦИЯ ДЛЯ ПРОВЕДЕНИЯ ГОЛОСОВАНИЯ (ТЕСТЫ)
+string conductElectionForTests(vector<Candidate>& candidates, vector<int>& votes) 
+{
+  if (candidates.size() == 0) 
+  {
+    return "NO CANDIDATES";
+  }
+  if (candidates.size() == 1) 
+  {
+    return "NO ELECTIONS";
+  }
+  if (votes.size() == 0) 
+  {
+    return "NOBODY VOTED";
+  }
+  if (count(votes.begin(), votes.end(), -1))
+  {
+    return "CHANGE CANDIDATES";
+  }
+
+  // Цикл голосования
+    for (int i = 0; i < votes.size(); i++) 
+    {
+      int choice = votes[i];
+      if (choice > candidates.size()) {
+          return "OUT OF BOUNDS";
+      }
+      // Увеличение количества голосов кандидата
+      candidates[choice - 1].votes++;
+    }
+
+  // Сортировка кандидатов по убыванию голосов
+  sort(candidates.begin(), candidates.end(), [](const Candidate& a, const Candidate& b) {
+      return a.votes > b.votes;
+  });
+
+  return candidates[0].name;
+}
+
+
+// ТЕСТЫ
+TEST(FunctionTesting, winner_test) {
+    // тест 1
+    std::vector<Candidate> candidates = {Candidate("Leo"), Candidate("Aboba"), Candidate("Griga")};
+    std::vector<int> votes = {1, 1, 1, 1, 3, 3, 1, 2, 3, 1, 2, 2};
+    EXPECT_EQ("Leo", conductElectionForTests(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, no_elections_test) {
+    // тест 2
+    std::vector<Candidate> candidates = {Candidate("Leo")};
+    std::vector<int> votes = {1, 1, 1, 1, 3, 3, 1, 2, 3, 1, 2, 2};
+    EXPECT_EQ("NO ELECTIONS", conductElectionForTests(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, no_candidates_test) {
+    // тест 3
+    std::vector<Candidate> candidates;
+    std::vector<int> votes = {1, 1, 1, 1, 3, 3, 1, 2, 3, 1, 2, 2};
+    EXPECT_EQ("NO CANDIDATES", conductElectionForTests(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, no_votes_test) {
+    // тест 4
+    std::vector<Candidate> candidates = {Candidate("Leo"), Candidate("Aboba"), Candidate("Griga") };
+    std::vector<int> votes;
+    EXPECT_EQ("NOBODY VOTED", conductElectionForTests(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, out_of_bounds_test) {
+    // тест 5
+    std::vector<Candidate> candidates = {Candidate("Leo"), Candidate("Aboba"), Candidate("Griga") };
+    std::vector<int> votes  = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1, 666, 34, 999};
+    EXPECT_EQ("OUT OF BOUNDS", conductElectionForTests(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
+TEST(FunctionTesting, change_candidates_test) {
+    // тест 6
+    std::vector<Candidate> candidates = {Candidate("Leo"), Candidate("Aboba"), Candidate("Griga") };
+    std::vector<int> votes  = {1, 1, 1, -1, 3, 3, 1, 2, 3, -1, 2, 2};
+    EXPECT_EQ("CHANGE CANDIDATES", conductElectionForTests(candidates, votes));
+    candidates.clear();
+    votes.clear();
+}
+
 void inputCandidateNames(vector<Candidate>& candidates, int numCandidates) 
 {
     for (int i = 0; i < numCandidates; i++) 
@@ -69,37 +168,45 @@ void inputCandidateNames(vector<Candidate>& candidates, int numCandidates)
         cin >> name;
         candidates.push_back(Candidate(name));
     }
-
 }
 
-int main() {
+int main(int argc, char **argv) 
+{
     // setlocale(LC_CTYPE, "Russian");
-    // Ввод количества кандидатов
-    int numCandidates;
-    cout << "Enter candidates count: ";
-    cin >> numCandidates;
 
-    // Ввод имён кандидатов
-    vector<Candidate> candidates;
-    inputCandidateNames(candidates, numCandidates);
+// ОСНОВНАЯ ЛОГИКА
+// Ввод количества кандидатов
+//     int numCandidates;
+//     cout << "Enter candidates count: ";
+//     cin >> numCandidates;
 
-    // Проведение голосования
-    while (true)
-    {
-        bool val = conductElection(candidates);
-        if (!val)
-        {
-            candidates.clear();
-            candidates.shrink_to_fit();
-            cout << "Enter candidates count: ";
-            cin >> numCandidates;
-            inputCandidateNames(candidates, numCandidates);
-        }
-        else
-        {
-            break;
-        }
-    }
+//     // Ввод имён кандидатов
+//     vector<Candidate> candidates;
+//     inputCandidateNames(candidates, numCandidates);
 
-    return 0;
+//     // Проведение голосования
+//     while (true)
+//     {
+//         bool val = conductElection(candidates);
+//         if (!val)
+//         {
+//             candidates.clear();
+//             candidates.shrink_to_fit();
+//             cout << "Enter candidates count: ";
+//             cin >> numCandidates;
+//             inputCandidateNames(candidates, numCandidates);
+//         }
+//         else
+//         {
+//             break;
+//         }
+//     }
+
+//     return 0;
+
+// ТЕСТЫ
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
+
+
