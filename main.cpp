28444818b9a34e225682a514b76ded04d9a23c44
Some-Mars <marsg242002@gmail.com>
2023-09-26 16:06:20 +0300

    Поиск длины самого длинного имени кандидата. Перевод

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-// РЎС‚СЂСѓРєС‚СѓСЂР° РґР»СЏ С…СЂР°РЅРµРЅРёСЏ РёРЅС„РѕСЂРјР°С†РёРё Рѕ РєР°РЅРґРёРґР°С‚Рµ
+// Структура для хранения информации о кандидате
 struct Candidate {
     string name;
     int votes;
@@ -15,63 +15,79 @@ struct Candidate {
         votes = 0;
     }
 };
-
-// Р¤СѓРЅРєС†РёСЏ РґР»СЏ РїСЂРѕРІРµРґРµРЅРёСЏ РіРѕР»РѕСЃРѕРІР°РЅРёСЏ
+// Searching for the length of the longest name of candidate
+int canditatesLongestName(vector<Candidate>& candidates){
+    int candidates_longest_name = 0;
+    for(int i = 0; i < candidates.size(); i++){
+        if (candidates[i].name.length() > candidates_longest_name){
+            candidates_longest_name = candidates[i].name.length();
+        }
+    }
+    return candidates_longest_name;
+}
+void outputElection(vector<Candidate>& candidates){
+    cout << "\nElection results:" << endl;
+    cout << endl;
+    // Вывод результатов
+    for (int i = 0; i < candidates.size(); i++) {
+        cout << "@ " << i + 1 << ". " << candidates[i].name << ": " << candidates[i].votes << " votes";
+        cout << "@" << endl;
+    }
+}
+// Функция для проведения голосования
 void conductElection(vector<Candidate>& candidates) {
-    cout << "Р“РѕР»РѕСЃРѕРІР°РЅРёРµ Р·Р° РІС‹Р±РѕСЂ СЃС‚Р°СЂРѕСЃС‚С‹!" << endl;
+    cout << "Voting for the choice of the headman!" << endl;
 
-    // Р’С‹РІРѕРґ СЃРїРёСЃРєР° РєР°РЅРґРёРґР°С‚РѕРІ
-    cout << "РљР°РЅРґРёРґР°С‚С‹:" << endl;
+    // Вывод списка кандидатов
+    cout << "Candidates:" << endl;
     for (int i = 0; i < candidates.size(); i++) {
         cout << i + 1 << ". " << candidates[i].name << endl;
     }
 
-    // Р¦РёРєР» РіРѕР»РѕСЃРѕРІР°РЅРёСЏ
+    // Цикл голосования
     while (true) {
-        cout << "Р’РІРµРґРёС‚Рµ РЅРѕРјРµСЂ РєР°РЅРґРёРґР°С‚Р°, Р·Р° РєРѕС‚РѕСЂРѕРіРѕ С…РѕС‚РёС‚Рµ РїСЂРѕРіРѕР»РѕСЃРѕРІР°С‚СЊ (0 - Р·Р°РєРѕРЅС‡РёС‚СЊ РіРѕР»РѕСЃРѕРІР°РЅРёРµ): ";
+        cout << "Enter the number of the candidate you want to vote for (0 - end voting):";
         int choice;
         cin >> choice;
 
         if (choice < 0 || choice > candidates.size()) {
-            cout << "РќРµРґРѕРїСѓСЃС‚РёРјС‹Р№ РІС‹Р±РѕСЂ!" << endl;
+            cout << "Invalid choice!" << endl;
             continue;
         }
         else if (choice == 0) {
             break;
         }
 
-        // РЈРІРµР»РёС‡РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° РіРѕР»РѕСЃРѕРІ РєР°РЅРґРёРґР°С‚Р°
+        // Увеличение количества голосов кандидата
         candidates[choice - 1].votes++;
     }
 
-    // РЎРѕСЂС‚РёСЂРѕРІРєР° РєР°РЅРґРёРґР°С‚РѕРІ РїРѕ СѓР±С‹РІР°РЅРёСЋ РіРѕР»РѕСЃРѕРІ
+    // Сортировка кандидатов по убыванию голосов
     sort(candidates.begin(), candidates.end(), [](const Candidate& a, const Candidate& b) {
         return a.votes > b.votes;
     });
 
-    // Р’С‹РІРѕРґ СЂРµР·СѓР»СЊС‚Р°С‚РѕРІ РіРѕР»РѕСЃРѕРІР°РЅРёСЏ
-    cout << "Р РµР·СѓР»СЊС‚Р°С‚С‹ РіРѕР»РѕСЃРѕРІР°РЅРёСЏ:" << endl;
-    for (int i = 0; i < candidates.size(); i++) {
-        cout << i + 1 << ". " << candidates[i].name << ": " << candidates[i].votes << " РіРѕР»РѕСЃРѕРІ" << endl;
-    }
+    // Вывод результатов голосования
+    // cout << "Результаты голосования:" << endl;
+    outputElection(candidates);
 }
 
 int main() {
-    // Р’РІРѕРґ РєРѕР»РёС‡РµСЃС‚РІР° РєР°РЅРґРёРґР°С‚РѕРІ
+    // Ввод количества кандидатов
     int numCandidates;
-    cout << "Р’РІРµРґРёС‚Рµ РєРѕР»РёС‡РµСЃС‚РІРѕ РєР°РЅРґРёРґР°С‚РѕРІ: ";
+    cout << "Enter the amount of candidates: ";
     cin >> numCandidates;
 
-    // Р’РІРѕРґ РёРјС‘РЅ РєР°РЅРґРёРґР°С‚РѕРІ
+    // Ввод имён кандидатов
     vector<Candidate> candidates;
     for (int i = 0; i < numCandidates; i++) {
         string name;
-        cout << "Р’РІРµРґРёС‚Рµ РёРјСЏ РєР°РЅРґРёРґР°С‚Р° #" << i + 1 << ": ";
+        cout << "Enter the name of candidate #" << i + 1 << ": ";
         cin >> name;
         candidates.push_back(Candidate(name));
     }
 
-    // РџСЂРѕРІРµРґРµРЅРёРµ РіРѕР»РѕСЃРѕРІР°РЅРёСЏ
+    // Проведение голосования
     conductElection(candidates);
 
     return 0;
