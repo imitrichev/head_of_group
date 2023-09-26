bda19760a85609a7081087d5460f4dca9d4b1a1a
Eleonora Yudina <elaudina03714@gmail.com>
2023-09-27 00:20:46 +0400

    new test 2

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -15,7 +15,24 @@ TEST(inputcandidates, FullListOfCandidates) {
 
 	string result = sstream.str();
 	ASSERT_EQ(result, "Voting for the choice of the headman!\nCandidates:\n1. Elya\n2. Mora\n3. Mura\n");
-	
+
+}
+
+TEST(circle, HaveInvalidChoices) {
+	vector<Candidate> candidates = {
+		Candidate("Elya"),
+		Candidate("Mora"),
+		Candidate("Mura")
+	};
+
+	istringstream in_stream("-1 6 5 0");
+	ostringstream out_stream;
+
+	circle(candidates, in_stream, out_stream);
+
+	ASSERT_EQ(candidates[0], votes, 0);
+	ASSERT_EQ(candidates[1], votes, 0);
+	ASSERT_EQ(candidates[2], votes, 0);
 }
 
 int main(int argc, char** argv)
