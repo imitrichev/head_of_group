e6f6cf5011f37870e554dd2494efd863c1ef3a78
Eleonora Yudina <elaudina03714@gmail.com>
2023-09-27 00:43:13 +0400

    test 3

--- test/test/test.cpp
+++ test/test/test.cpp
@@ -30,9 +30,28 @@ TEST(circle, HaveInvalidChoices) {
 
 	circle(candidates, out_stream, in_stream);
 
+
 	ASSERT_EQ(candidates[0].votes, 0);
 	ASSERT_EQ(candidates[1].votes, 0);
 	ASSERT_EQ(candidates[2].votes, 0);
+}
+
+TEST(outputcandidates, HaveCorrectChoices) {
+	vector<Candidate> candidates = {
+		Candidate("Elya"),
+		Candidate("Mora")
+	};
+
+	istringstream in_stream("1 Y 0");
+	ostringstream out_stream;
+
+	circle(candidates, out_stream, in_stream);
+
+	stringstream sstream;
+	outputcandidates(candidates, sstream);
+	string result = sstream.str();
+	
+	ASSERT_EQ(result, "Voting results:\n1. Elya: ", candidates[0].votes, " voices\n2.Mora: ", candidates[1].votes, " voices\n");
 }
 
 int main(int argc, char** argv)
