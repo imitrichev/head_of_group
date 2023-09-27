a29b6ea3b0869c6f6c79ed3a43d7b695b3de1287
Some-Mars <marsg242002@gmail.com>
2023-09-27 15:14:49 +0300

    Тесты сделаны. Отправка на Github Actions.

--- tests.cpp
+++ tests.cpp
@@ -14,6 +14,40 @@ TEST(CandidateTest, Creation){
     EXPECT_EQ(candidate.votes, 0);
 }
 
+TEST(ElectionTest, ConductElection){
+    vector<Candidate> candidates;
+    candidates.push_back(Candidate("Candidate1"));
+    candidates.push_back(Candidate("Candidate2"));
+
+    std::istringstream input_stream("1\n2\n0\n");
+    std::cin.rdbuf(input_stream.rdbuf());
+
+    conductElection(candidates);
+
+    EXPECT_EQ(candidates[0].votes + candidates[1].votes, 2);
+}
+TEST(ElectionTest, VoteForCandidate){
+    vector<Candidate> candidates;
+    candidates.push_back(Candidate("Candidate1"));
+    candidates.push_back(Candidate("Candidate2"));
+
+    std::istringstream input_stream("1\n1\n1\n0\n");
+
+    conductElection(candidates);
+
+    EXPECT_EQ(candidates[0].votes, 3);
+}
+TEST(ElectionTest, ZeroCandidates){
+    vector<Candidate> candidates;
+
+    std::istringstream input_stream("0\n");
+    std::cin.rdbuf(input_stream.rdbuf());
+
+    conductElection(candidates);
+
+    EXPECT_TRUE(candidates.empty());
+}
+
 int main(int argc, char* argv[])
 {
     ::testing::InitGoogleTest(&argc, argv);
