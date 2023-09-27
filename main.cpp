d520b47c4abc447b013c6dafa852a91555e8658d
DashaNiki <99253914+DashaNiki@users.noreply.github.com>
2023-09-27 16:05:05 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@
 using namespace std;
 // Структура для хранения информации о кандидате
 struct Candidate {
-    std::string name;
-    std::string surname;
+     string name;
+     string surname;
     int votes;
 
-    Candidate(std::string n, std::string s) {
+    Candidate( string n,  string s) {
         name = n;
         surname = s;
         votes = 0;
@@ -20,13 +20,13 @@ struct Candidate {
 };
 
 // Функция для проведения голосования
-std::string conductElection(std::vector<Candidate>& candidates, std::vector<int>& votes) {
-    std::cout << "Elections of captain!" << std::endl;
+ string conductElection( vector<Candidate>& candidates,  vector<int>& votes) {
+     cout << "Elections of captain!" <<  endl;
 
     // Вывод списка кандидатов
-    std::cout << "Candidates:" << std::endl;
+     cout << "Candidates:" <<  endl;
     for (int i = 0; i < candidates.size(); i++) {
-        std::cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name << std::endl;
+         cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name <<  endl;
     }
 
     if (candidates.size() == 0) {
@@ -58,25 +58,19 @@ std::string conductElection(std::vector<Candidate>& candidates, std::vector<int>
     });
 
     // Вывод результатов голосования
-    std::cout << "Election results:" << std::endl;
+     cout << "Election results:" <<  endl;
     for (int i = 0; i < candidates.size(); i++) {
-        std::cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name << ": " << candidates[i].votes << " votes" << std::endl;
+         cout << i + 1 << ". " << candidates[i].surname << " "<< candidates[i].name << ": " << candidates[i].votes << " votes" <<  endl;
     }
 
     // Возвращаем фамилию и имя кандидата победителя
     return candidates[0].surname + " " + candidates[0].name;
 }
 
-//Тестирование конструктора Candidate:
-TEST(CandidateTest, Constructor) {
-    Candidate candidate("Иванов");
-    EXPECT_EQ(candidate.name, "Иванов");
-    EXPECT_EQ(candidate.votes, 0);
-}
 TEST(FunctionTesting, test_elections) {
     // тест 1
-    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
-    std::vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
+     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+     vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("Sidorenko Nikita", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
@@ -84,8 +78,8 @@ TEST(FunctionTesting, test_elections) {
 
 TEST(FunctionTesting, test_elections_single_candidate) {
     // тест 2
-    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko") };
-    std::vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
+     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko") };
+     vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("NO ELECTIONS", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
@@ -93,8 +87,8 @@ TEST(FunctionTesting, test_elections_single_candidate) {
 
 TEST(FunctionTesting, test_elections_no_candidates) {
     // тест 3
-    std::vector<Candidate> candidates;
-    std::vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
+     vector<Candidate> candidates;
+     vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("NO CANDIDATES", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
@@ -102,8 +96,8 @@ TEST(FunctionTesting, test_elections_no_candidates) {
 
 TEST(FunctionTesting, test_elections_no_votes) {
     // тест 4
-    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
-    std::vector<int> votes;
+     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+     vector<int> votes;
     EXPECT_EQ("NO VOTES", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
@@ -111,8 +105,8 @@ TEST(FunctionTesting, test_elections_no_votes) {
 
 TEST(FunctionTesting, test_elections_illegal_votes) {
     // тест 5
-    std::vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
-    std::vector<int> votes  = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1, 50, 46, 78};
+     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+     vector<int> votes  = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1, 50, 46, 78};
     EXPECT_EQ("ILLEGAL VOTE", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
@@ -120,22 +114,6 @@ TEST(FunctionTesting, test_elections_illegal_votes) {
 
 
 int main(int argc, char **argv) {
-     // Ввод количества кандидатов
-    //int numCandidates;
-    //cout << "Введите количество кандидатов: ";
-    //cin >> numCandidates;
-
-    // Ввод имён кандидатов
-    //vector<Candidate> candidates;
-    //for (int i = 0; i < numCandidates; i++) {
-        //string name;
-        //cout << "Введите фамилию и имя кандидата #" << i + 1 << ": ";
-        //cin >> name;
-        //candidates.push_back(Candidate(name));
-    //}
-
-    // Проведение голосования
-    //conductElection(candidates);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
