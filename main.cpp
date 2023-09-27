d7b78e27323f525970aafc86efd641a85c838d1a
DashaNiki <99253914+DashaNiki@users.noreply.github.com>
2023-09-27 16:12:11 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -66,47 +66,43 @@ struct Candidate {
     // Возвращаем фамилию и имя кандидата победителя
     return candidates[0].surname + " " + candidates[0].name;
 }
-
+// Тест проверяет правильность работы функции проведения выборов в обычных условиях с несколькими кандидатами и голосами.
 TEST(FunctionTesting, test_elections) {
-    // тест 1
      vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
      vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("Sidorenko Nikita", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
 }
-
+// Тест проверяет, что функция правильно обрабатывает случай, когда всего один кандидат
 TEST(FunctionTesting, test_elections_single_candidate) {
-    // тест 2
-     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko") };
+         vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko") };
      vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("NO ELECTIONS", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
 }
 
+// Тест проверяет, что функция правильно обрабатывает случай, когда нет кандидатов.
 TEST(FunctionTesting, test_elections_no_candidates) {
-    // тест 3
-     vector<Candidate> candidates;
+       vector<Candidate> candidates;
      vector<int> votes = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1};
     EXPECT_EQ("NO CANDIDATES", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
 }
-
+// Тест проверяет, что функция правильно обрабатывает случай, когда нет голосов.
 TEST(FunctionTesting, test_elections_no_votes) {
-    // тест 4
-     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
-     vector<int> votes;
+    vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+    vector<int> votes;
     EXPECT_EQ("NO VOTES", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
 }
-
+// Тест проверяет, что функция правильно обрабатывает случай, когда имеются незаконные голоса (голоса за несуществующих кандидатов).
 TEST(FunctionTesting, test_elections_illegal_votes) {
-    // тест 5
-     vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
-     vector<int> votes  = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1, 50, 46, 78};
+    vector<Candidate> candidates = {Candidate("Nikita", "Sidorenko"), Candidate("Ryan", "Gosling"), Candidate("Mikhail", "Sidorenko") };
+    vector<int> votes  = {1, 1, 1, 2, 2, 3, 1, 2, 3, 1, 1, 1, 50, 46, 78};
     EXPECT_EQ("ILLEGAL VOTE", conductElection(candidates, votes));
     candidates.clear();
     votes.clear();
